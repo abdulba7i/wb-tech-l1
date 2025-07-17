1c660c838b1dff5a8d68804872ef8179c914b8c6
AbdulBari <bay7amov@gmail.com>
2025-07-17 15:32:33 +0300

    l1.20

---
+++ l1.20/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	text, _ := reader.ReadString('\n')
+
+	text = strings.TrimSpace(text)
+	fmt.Println(reversedText(text))
+}
+
+func reversedText(text string) string {
+	words := strings.Split(text, " ")
+
+	for i, j := 0, len(words)-1; i < j; i, j = i+1, j-1 {
+		words[i], words[j] = words[j], words[i]
+	}
+
+	return strings.Join(words, " ")
+}
